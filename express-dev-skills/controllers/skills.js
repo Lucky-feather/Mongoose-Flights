b202ab21d2037180d2de7e427738433b8b94cc0d
LH <lindsaynhenderson@gmail.com>
2022-09-22 16:16:20 -0500

    complete step three of lab

--- express-dev-skills/controllers/skills.js
+++ express-dev-skills/controllers/skills.js
@@ -11,14 +11,41 @@ function index(req, res) {
   .catch(error => { // If there's an error, console.log it and redirect back home!
     console.log(error)
     res.redirect('/')
-
   })
 }
 function newSkill(req, res) {
   res.render('skills/new')
 }
 
+function create(req, res) {
+  console.log(req.body)
+  Skill.create(req.body)
+  .then(skill => {
+		// Notice we are doing a redirect here!
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
+function show(req, res) {
+  Skill.findById(req.params.id)
+  .then(skill => {
+    res.render('skills/show', {
+      skill: skill
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 export {
   index,
-  newSkill as new
+  newSkill as new,
+  create,
+  show
 }
