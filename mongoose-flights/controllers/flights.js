373ffd83716cef5678ea743633b1a39c1918c511
LH <lindsaynhenderson@gmail.com>
2022-09-25 19:06:22 -0500

    create page with index of all flights

--- mongoose-flights/controllers/flights.js
+++ mongoose-flights/controllers/flights.js
@@ -2,13 +2,41 @@ import { Flight } from "../models/flight.js"
 
 function newFlight (req, res){
   console.log('new flight')
-  res.render("flights/new", {
+  res.render('flights/new', {
   title: "Add Flight",
+  })
+}
+
+function create(req, res) {
+  Flight.create(req.body)
+  .then(flight => {
+    console.log(flight)
+    res.redirect(`/flights/new`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All Flights'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
   })
 }
 
 
 
 export {
-  newFlight as new
+  newFlight as new,
+  create,
+  index
 }
--- mongoose-flights/routes/flights.js
+++ mongoose-flights/routes/flights.js
@@ -4,10 +4,13 @@ import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
 //GET /flights
-
+router.get('/', flightsCtrl.index)
 
 // GET /flights/new
 router.get('/new', flightsCtrl.new)
+
+// POST /flights
+router.post('/', flightsCtrl.create)
 
 export {
 	router
